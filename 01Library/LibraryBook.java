4361944d3150c26e48e038488e7069b30b0cde8e
Anthony Hom <anthonyhom2@hotmail.com>
2016-12-04 00:26:48 -0500

    Fixed abstract methods and updated compareTo

--- 01Library/LibraryBook.java
+++ 01Library/LibraryBook.java
@@ -10,22 +10,24 @@ abstract class LibraryBook extends Book implements Comparable<LibraryBook>{
 		return callNumber;
 	}
 
-	public String setcallNum(){
+	public void setcallNum(){
 		callNumber = number;
 	}
-
+	
 	public abstract void checkout(String patron, String due);
 	public abstract void returned();
 	public abstract String circulationStatus();
-/*
-	public String compareTo(LibraryBook num){
-		if (num implements LibraryBook){
-	LibraryBook other = LibraryBook num;
-}
-*/
 
-else {return -1;}
-	}
+// Define the compareTo method
+	public int compareTo(LibraryBook book){
+		if (callNumber().compareTo(book.getcallNum()) < 0){
+			return -1;
+		}
+		else if (callNumber().compareTo(book.getcallNum()) > 0){
+			return 1;
+		}
+		else{return 0;}
+}
 
 	public String toString(){
 		return super.toString() + ". The book's call number is " + callNumber;
