127b2e874471adfc005f57648d95313f4e0938f8
Anthony Hom <anthonyhom2@hotmail.com>
2016-12-05 05:52:22 -0500

    Need to add the two requirements

--- APCSQuestion.java
+++ APCSQuestion.java
@@ -1,7 +1,7 @@
 public boolean isDone(){
 	for (int row = 0; row < side; row++){
 		for (int column = 0; column < side; column++){
-			if (value[row][column] > value[row][column + 1] && value[row][column] == 0){
+			if (value[row][column] > value[row][column + 1] && value[row][column] != 0){
 				return false;
 }
 			else { return true;}
