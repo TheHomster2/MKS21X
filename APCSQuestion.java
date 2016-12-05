f2fcc4baa5eec24138cc71d6234eefa095c6ffee
Anthony Hom <anthonyhom2@hotmail.com>
2016-12-05 05:49:58 -0500

    Finish

---
+++ APCSQuestion.java
@@ -0,0 +1,10 @@
+public boolean isDone(){
+	for (int row = 0; row < side; row++){
+		for (int column = 0; column < side; column++){
+			if (value[row][column] > value[row][column + 1] && value[row][column] == 0){
+				return false;
+}
+			else { return true;}
+}
+}
+}
