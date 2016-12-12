8685fdcdb7b41e9f325c6bc618504d89e3ad7a5a
Anthony Hom <anthonyhom2@hotmail.com>
2016-12-11 22:09:23 -0500

    Added tozip and changed checkSum because of ascii value conversion prob

--- 03Barcode/Barcode.java
+++ 03Barcode/Barcode.java
@@ -24,7 +24,7 @@ public class Barcode implements Comparable<Barcode>{
 
 // postcondition: Creates a copy of a bar code. In Java a clone method builds a second copy of an object and returns it. 
   public Barcode clone(){
-	Barcode clone = new Barcode (zip);
+	Barcode clone = new Barcode (_zip);
 	return clone;
 }
 
@@ -32,17 +32,27 @@ public class Barcode implements Comparable<Barcode>{
 // postcondition: computes and returns the check sum for _zip
 // loop through the string and take the ASCII value to determine the sum... We'd use charAt or substring 
 // checkSum() is used to indicate each object's unique sum
+//checkSum() also puts in checkDigit for us
+//realized I haven't converted char to ASCII value
   private int checkSum(){
+  	/*
 	int sum = 0;
-	for (int index = 0; index < _zip.length(); index++){
-	if (_zip.charAt(index) < 58 && _zip.charAt(index) > 47){
-	sum = sum + _zip.charAt(index) - 48;
+	for (int index = 0; index < zip.length(); index++){
+	if (zip.charAt(index) < 58 && zip.charAt(index) > 47){
+	sum = sum + zip.charAt(index) - 48;
 }
 }
+*/
+	int count1 = Integer.parseInt(_zip.substring(0,1));
+	int count2 = Integer.parseInt(_zip.substring(1,2));
+	int count3 = Integer.parseInt(_zip.substring(2,3));
+	int count4 = Integer.parseInt(_zip.substring(3,4));
+	int count5 = Integer.parseInt(_zip.substring(4,5));
+	int sum = count1 + count2 + count3 + count4 + count5;
 	int checkDigit = sum % 10;
 	_checkDigit = checkDigit;
 	return checkDigit;
-	
+}
 	
 		
 
@@ -67,7 +77,7 @@ public class Barcode implements Comparable<Barcode>{
 // Don't compare the barcode with the string 
 //Compares the barcode with the numerical order. In this case it takes the first five digits excluding the check digit
   public int compareTo(Barcode other){
-    return (toString().substring(0,6).compareTo(other.toString().substring(0,6)))
+    return (toString().substring(0,6).compareTo(other.toString().substring(0,6)));
 }
 
 	public static String toCode(String zip){
@@ -76,7 +86,7 @@ public class Barcode implements Comparable<Barcode>{
 			throw new IllegalArgumentException("Inappropriate zip length!");
 		}
 		else{
-			for(int digitindex = 0; digitindex < zip.length; digitindex++){
+			for(int digitindex = 0; digitindex < zip.length(); digitindex++){
 				for (int index = 0; index < bars.length; index++){
 					if (index == Integer.parseInt(zip.substring(digitindex, digitindex + 1))){
 						result += bars[index] + "|";
@@ -87,15 +97,54 @@ public class Barcode implements Comparable<Barcode>{
 }
 }
 
-	public static String toZip(String code){
-		String result = "";
 
+
+	public String toZip(String code){
+	String result = "";
+	if(code.length() != 32){
+	    throw new IllegalArgumentException("Inappropriate bar length");}
+	else if(code.charAt(0) != '|' || code.charAt(31) != '|'){
+	    throw new IllegalArgumentException("Where are the starting and ending bars?");}
+	else{
+		int i = 1;
+	    while (i < code.length()-5){
+		String zip = code.substring(i,i+5);
+		switch(zip){
+		case  ":::||": result += 1;
+		    break;
+		case "::|:|": result += 2;
+		    break;
+		case "::||:": result += 3;
+		    break;
+		case ":|::|": result += 4;
+		    break;
+		case ":|:|:": result += 5;
+		    break;
+		case ":||::": result += 6;
+		    break;
+		case "|:::|": result += 7;
+		    break;
+		case "|::|:": result += 8;
+		    break;
+		case "|:|::": result += 9;
+		    break;
+		case "||:::": result += 0;
+		    break;
+		}
+        	i += 5;
+	    }
+	    return result;
 	}
 
+}
+}
 
-/*What data should a Barcode object store? The Barcode should store the String zipcode
+
+
+/*
+What data should a Barcode object store? The Barcode should store the String zipcode
 What methods does a Barcode need to be minimally useful? (Public Methods) Constructor that takes the zipcode, toString, compareTo
 What methods could be useful for other classes? (static methods) -> http://stackoverflow.com/questions/2671496/java-when-to-use-static-methods
 toZip(String code)
 toCode(String zip)
-
+*/
