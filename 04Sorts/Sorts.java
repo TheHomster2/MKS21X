1629f931df76518505112db11f79dc127cd716fe
Anthony Hom <anthonyhom2@hotmail.com>
2016-12-14 05:34:31 -0500

    Git probs need to overwrite an old index for a new

--- 04Sorts/Sorts.java
+++ 04Sorts/Sorts.java
@@ -1,36 +1,34 @@
 /*
-[   64 25 12 22 11] // this is the initial, starting state of the array 
-[11    25 12 22 64] // sorted sublist = {11} 
-[11 12    25 22 64] // sorted sublist = {11, 12} 
-[11 12 22    25 64] // sorted sublist = {11, 12, 22} 
-[11 12 22 25    64] // sorted sublist = {11, 12, 22, 25} 
+[   64 25 12 22 11] // this is the initial, starting state of the array
+[11    25 12 22 64] // sorted sublist = {11}
+[11 12    25 22 64] // sorted sublist = {11, 12}
+[11 12 22    25 64] // sorted sublist = {11, 12, 22}
+[11 12 22 25    64] // sorted sublist = {11, 12, 22, 25}
 [11 12 22 25 64   ] // sorted sublist = {11, 12, 22, 25, 64}
 */
 
 public class Sorts{
-  /**EDIT THIS METHOD TO MATCH YOUR NAME 
+  /**EDIT THIS METHOD TO MATCH YOUR NAME
   */
   public static String name(){
-    return "01.Hom.Anthony"; 
+    return "01.Hom.Anthony";
   }
 
-  /**Selection sort of an int array. 
+  /**Selection sort of an int array.
   *Upon completion, the elements of the array will be in increasing order.
   *@param data  the elements to be sorted.
   */
-  public static void selectionSort(int[] data){ 
-	int minimum = data[0];
-	int tracker = data[0];
-	for (int index = 1; index < data.length - 1; index++){
-		if (data[index] < minimum){
-			minimum = data[index];
-			data[index] = tracker;	
-			
-			
-			
+  public static void selectionSort(int[] data){
+    int minimum = data[0];
+    int tracker = data[0];
+    for (int index = 1; index < data.length - 1; index++){
+        if (data[index] < minimum){
+            minimum = data[index];
+            data[index] = tracker;   
+           
+           
+           
 }
  }
 
 }
-
-
