466ac115dca54c6b09a898675b1dc61d8dba45c5
Anthony Hom <anthonyhom2@hotmail.com>
2016-12-20 05:44:46 -0500

    Done

--- Temperature.java
+++ Temperature.java
@@ -1,10 +1,10 @@
-public class Tempurature{
+public class Temperature{
 	public static double CtoF(double t){
-	return (t * (9/5)) + 32;
+	return t * (9.0/5) + 32;
 }
 
 	public static double FtoC(double t){
-	return ((t - 32) * (5/9));
+	return ((t - 32) * (5.0/9));
 }
 
 	public static void main (String[]args){
