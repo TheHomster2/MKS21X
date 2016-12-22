6c3dcd9877330c6684ad41476ad3d901400c06b2
Anthony Hom <anthonyhom2@hotmail.com>
2016-12-21 23:20:42 -0500

    Added the fxns but still need to understand why input would not take in double only takes in ints

--- GUI/Temp.java
+++ GUI/Temp.java
@@ -1,33 +1,74 @@
 import javax.swing.*;
 import java.awt.*;
-public class Temp extends JFrame{
+import java.awt.event.*;
+public class Temp extends JFrame implements ActionListener{
  
  private Container pane;
- private JButton b;
- private JLabel l;
  private JTextField t;
- private JCheckBox c;
+ private JButton b;
+ private JButton b2;
+ private JTextField c;
+ private JLabel a;
 
- public Window2() {
-     this.setTitle("My first GUI");
+ public Temp() {
+     this.setTitle("Temperature Conversion Window");
      this.setSize(600,400);
      this.setLocation(100,100);
      this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 
-     b = new JButton("Do Nothing");
-     l = new JLabel("This is AWESOME! (lies)",null,JLabel.CENTER);
-     t = new JTextField(12);
-     c = new JCheckBox("Overclock!!!");
-     pane.add(l);
-     pane.add(b);
+     pane = this.getContentPane();
+     pane.setLayout(new BoxLayout(pane, BoxLayout.Y_AXIS));
+
+     JLabel a = new JLabel();
+     a.setText("Enter the temperature");
+     t = new JTextField(5);
+
+     JButton b = new JButton("Enter temperature to convert to Fahrenheit");
+      b.addActionListener(this);
+      b.setActionCommand("CtoF");
+      JButton b2 = new JButton("Enter temperature to convert to Celsius");
+      b2.addActionListener(this);
+      b2.setActionCommand("FtoC");
+
+     c = new JTextField();
+     c.setText("Output");
+
+     pane.add(a);
      pane.add(t);
+     pane.add(b);
+     pane.add(b2);
      pane.add(c);
- }
+  }
+
+ public void actionPerformed(ActionEvent e){
+   String event = e.getActionCommand();
+   if(event.equals("CtoF")){
+     String s = t.getText();
+     double input = Integer.parseInt(s);
+    double output = CtoF (input);
+    c.setText("This is " + output + " degrees Fahrenheit");
+   }
+   else if(event.equals("FtoC")){
+    String s = t.getText();
+     double input = Integer.parseInt(s);
+    double output = FtoC (input);
+    c.setText("This is " + output + " degrees Celsius");
+   }
+   }
+
+public static double CtoF(double t){
+    return t * (9.0/5) + 32;
+}
+
+public static double FtoC(double t){
+    return ((t - 32) * (5.0/9));
+}
 
   public static void main(String[] args) {
-     Window2 g = new Window2();
+     Temp g = new Temp();
      g.setVisible(true);
   }
 }
+
 
 
