78016134a5284212dc9dc89ca19b6e12d4e3fbaf
Anthony Hom <anthonyhom2@hotmail.com>
2016-12-22 23:52:57 -0500

    Filled in insertion method using two for loops and simplified the method

--- 04Sorts/Sorts.java
+++ 04Sorts/Sorts.java
@@ -36,8 +36,10 @@ public class Sorts{
   public static void insertionSort(int[] data){
     for (int index = 1; index < data.length; index++){
         int temp = data[index];
-	int 
-        
+	      for (int other = index; other >= 0 && temp < data[other -1]; other--){
+          data[other] = data[other - 1];
+        }
+        data[other] = temp;
     }
   }
 
